6a6a77e90f872c405fedb4970efd41f1949960bf
Temur <timurzaitov1998@gmail.com>
2023-08-29 18:48:24 +0500

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -38,8 +38,8 @@ public class RemoteFeedLoader {
         client.get(from: url) { result  in
             switch result {
             case let .success(data, _):
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from : data) {
+                    completion(.success(root.items))
                 }else {
                     completion(.failure(.invalidData))
                 }
@@ -50,3 +50,6 @@ public class RemoteFeedLoader {
     }
 }
 
+private struct Root: Decodable {
+    let items: [FeedItem]
+}
