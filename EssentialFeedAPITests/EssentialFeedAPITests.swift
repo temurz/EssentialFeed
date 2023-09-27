0fb54db3875c5b4b07ad49bb569453ce28da72d6
Temur <timurzaitov1998@gmail.com>
2023-09-27 11:29:37 +0200

    Extract test setup to helper method

--- EssentialFeedAPITests/EssentialFeedAPITests.swift
+++ EssentialFeedAPITests/EssentialFeedAPITests.swift
@@ -33,20 +33,9 @@ final class EssentialFeedAPITests: XCTestCase {
 //    }
     
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerUrl ,client: client)
         
-        let exp = expectation(description: "Wait for connection")
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
         
-        wait(for: [exp], timeout: 5.0)
-        
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed!")
             
@@ -67,6 +56,23 @@ final class EssentialFeedAPITests: XCTestCase {
 
     
     //MARK: - Helpers
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerUrl ,client: client)
+        
+        let exp = expectation(description: "Wait for connection")
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
+    }
     
     private func expectedItem(at index: Int) -> FeedItem {
         return FeedItem(id: id(at: index), description: description(at: index), location: location(at: index), imageURL: imageURL(at: index))
